ee404392da0fbf3eef357f5e15de7328c027a778
Rizvan Dev <rizvan.ahmedov@outlook.com>
2021-03-15 00:38:46 +0400

    добавил модальное окно с формой к блоку feedback и адаптировал его

--- js/script.js
+++ js/script.js
@@ -12,15 +12,16 @@ document.querySelector('.header-menu').addEventListener('click', function (e) {
 
 let modal = document.getElementById("header__popup");
 let btn = document.getElementById("my-modal");
+let close = document.getElementsByClassName("header__popup-close")[0];
+
 
 btn.onclick = function () {
-   modal.style.margin = "0px", modal.style.transition = "all .7s";
+   modal.style.margin = "0px", modal.style.transition = "all .5s";
 };
-window.onclick = function (event) {
-   if (event.target == modal) {
-      modal.style.margin = "-1500px  0 0 0", modal.style.transition = ".5s";
-   }
+close.onclick = function () {
+   modal.style.margin = "-1500px 0 0 0", modal.style.transition = ".3s";
 };
+
 
 new Swiper('.about__slider.swiper-container', {
    navigation: {
@@ -40,3 +41,19 @@ new Swiper('.about__slider.swiper-container', {
    },
    speed: 1000,
 });
+
+let popup = document.getElementById("feedback");
+let button = document.getElementById("feedback-btn");
+let exit = document.getElementsByClassName("feedback__exit")[0];
+
+button.onclick = function () {
+   popup.style.opacity = "1", popup.style.visibility = "visible", popup.style.transition = "all 0.8s";
+};
+exit.onclick = function () {
+   popup.style.opacity = "0", popup.style.visibility = "hidden", popup.style.transition = ".6s";
+};
+window.onclick = function (event) {
+   if (event.target == popup) {
+      popup.style.opacity = "0", popup.style.visibility = "hidden", popup.style.transition = "all .6s";
+   }
+};
